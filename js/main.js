155475c10928f5448f79a5e5990d1ef4490f74ae
Giovanni Maiorana <116109323+Giovannimaiorana@users.noreply.github.com>
2023-03-14 16:03:19 +0100

    esercizio + parte bonus

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'che belle le semplificazioni'
+      message: 'Che belle le semplificazioni',
+      image:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png'
+
     }
   }
 }).mount('#app')
